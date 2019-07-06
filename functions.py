48dab5d035be54db322e2ce3acb45166dc6a833f
Igor Theotônio <igor.c.a.theotonio@gmail.com>
2019-07-05 21:31:37 -0300

    Implementação da função de testes Ackley

--- functions.py
+++ functions.py
@@ -9,3 +9,10 @@ def sphere(x):
 def rastringin(x):
     y = 10 * len(x) + sum(map(lambda i: i ** 2 - 10 * np.cos(2 * np.pi * i), x))
     return y
+
+def ackley(x, a=20, b=0.2, c=2*np.pi):
+    x = np.array(x)
+    d = len(x)
+    sum_sq_term = -a * np.exp(-b * np.sqrt(sum(x*x) / d))
+    cos_term = -np.exp(sum(np.cos(c*x) / d))
+    return a + np.exp(1) + sum_sq_term + cos_term
