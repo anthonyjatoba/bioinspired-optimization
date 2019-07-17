80513aa7da7fb68a9abc3f587ee76f4ee2b297b5
Igor Theotônio <igor.c.a.theotonio@gmail.com>
2019-07-16 21:04:18 -0300

    Implementação de seleção por roleta e aleatória, bem como gráficos correspondentes. Faz-se necessário revisar a implementação aleatórioa.

--- ga.py
+++ ga.py
@@ -2,6 +2,8 @@ import struct
 import random
 import textwrap
 import numpy as np
+from random import randint, uniform
+from math import isnan
 from functions import sphere, rastringin, ackley
 
 
@@ -25,7 +27,12 @@ class GA:
         if cx_strategy == 'one-point':
             self.cx_strategy = self.one_point_cx
         # TODO: two-point crossover
-        if sel_strategy == 'elitist':
+        
+        if sel_strategy == 'roulette':
+            self.sel_strategy = self.roulette_selection
+        elif sel_strategy == 'random':
+            self.sel_strategy = self.random_selection
+        else:
             self.sel_strategy = self.elitist_selection
         # TODO: roulette, random
 
@@ -48,9 +55,20 @@ class GA:
         for ind in population:
             # Breaks the string into strings of 32 bits (a float binary)
             binary_values = textwrap.wrap(ind['chromosome'], 32)
+            
+            for i in range(len(binary_values)):
+                if binary_values[i][1] == '1' and binary_values[i][2] == '1' and binary_values[i][3] == '1' and
+                binary_values[i][4] == '1' and binary_values[i][5] == '1' and binary_values[i][6] == '1' and
+                binary_values[i][7] == '1' and binary_values[i][8] == '1':
+                    v1 = list(binary_values[i])
+                    v1[8] = '0'
+                    binary_values[i] = ''.join(v1)
+                    
             # Converts the binaries to floats
             float_values = [bin_to_float(v) for v in binary_values]
             ind['fitness'] = self.fun(float_values)
+            if isnan(ind['fitness']):
+                print(ind)
         return population
 
     def mutate(self, population):
@@ -63,7 +81,6 @@ class GA:
                 else:
                     chromosome_mutated += c
             individual['chromosome'] = chromosome_mutated
-        self.evaluate(population)
         return population
 
     def one_point_cx(self, ind1, ind2):
@@ -78,8 +95,12 @@ class GA:
         ch2_chromosome = ind2['chromosome'][0:point] + \
             ind1['chromosome'][point:ch_len]
 
-        child1 = {'chromosome': ch1_chromosome, 'fitness': -np.inf}
-        child2 = {'chromosome': ch2_chromosome, 'fitness': -np.inf}
+        child1 = {'chromosome': ch1_chromosome, 'fitness': -np.inf,
+                  'inv_fitness': -np.inf, 'norm_fitness': -np.inf, 
+                  'cumulative_fitness': -np.inf, 'chosen': False}
+        child2 = {'chromosome': ch2_chromosome, 'fitness': -np.inf,
+                  'inv_fitness': -np.inf, 'norm_fitness': -np.inf,
+                  'cumulative_fitness': -np.inf, 'chosen': False}
 
         return child1, child2
 
@@ -93,6 +114,43 @@ class GA:
         """ Selects the (pop_size) best individuals from a population"""
         population = self.sort_pop(population)
         return population[0:self.pop_size]
+    
+    def roulette_selection(self, population):
+        """ Selects (pop_size) best individuals from a population using roulette selection"""
+        
+        total_fitness = 0
+        cumulative_fitness = 0
+        new_population = []
+        for individual in population:
+            individual['inv_fitness'] = 1/(individual['fitness'] + 1)
+            total_fitness += individual['inv_fitness']
+            
+        for individual in population:               
+            individual['norm_fitness'] = individual['inv_fitness']/total_fitness
+            cumulative_fitness += individual['norm_fitness']
+            individual['cumulative_fitness'] = cumulative_fitness
+        
+        while len(new_population) != self.pop_size:
+            rand = uniform(0, 1)
+            for individual in population:
+                if rand <= individual['cumulative_fitness']:
+                    if not individual['chosen']:
+                        new_population.append(individual)
+                    break
+        
+        return new_population
+    
+    def random_selection(self, population):
+        """ Randomly selects the (pop_size) individuals"""
+        new_population = []
+        
+        while len(new_population) != self.pop_size:
+            rand = randint(0, len(population) - 1)
+            if not population[rand]['chosen']:
+                new_population.append(population[rand])
+                population[rand]['chosen'] = True
+       
+        return new_population
 
     def crossover(self, population):
         """ Performs crossover into a population until a offspring with (pop_size) individuals is completed """
@@ -107,25 +165,42 @@ class GA:
 
     def run(self):
         # Initialize the population
-        population = [{'chromosome': self.gen_individual(), 'fitness': -np.inf}
+        population = [{'chromosome': self.gen_individual(), 'fitness': -np.inf,
+                       'inv_fitness': -np.inf, 'norm_fitness': -np.inf, 
+                       'cumulative_fitness': -np.inf, 'chosen': False}
                       for x in range(self.pop_size)]
-
-        #Calculate the fitness for each individual
+        
+        # Initializing array for best fitness in each generation
+        gen_best_fitnesses = []
+        
+        # Calculate the fitness for each individual in the initial population
         self.evaluate(population)
-
+        
+        # Sorts initial population
+        population = self.sort_pop(population)
+            
         for g in range(self.generations):
-
-            # Sorts population
-            population = self.sort_pop(population)
-
             # Generating the offspring
             offspring = self.crossover(population)
 
             # Selecting the new population from the old + offpring
-            population = self.elitist_selection(population + offspring)
-
+            population = self.sel_strategy(population + offspring)
+            
+            # Reseting statuses for next generation
+            for i in range(self.pop_size):
+                population[i]['chosen'] = False
+            
             if self.mt_prob > 0:
                 # Perform evolutionary operations (mutation, etc.)
                 population = self.mutate(population)
-
-        return population[0]['fitness']
+            
+            # Calculate the fitness for each individual
+            self.evaluate(population)
+            
+            # Sorts population
+            population = self.sort_pop(population)
+            
+            # Saves generation best fitness
+            gen_best_fitnesses.append(population[0]['fitness'])
+            
+        return population[0]['fitness'], gen_best_fitnesses
